601432329f3bf28da26fd296c98db5176549a47a
InformaticsHistorySASA <informatics.history.sasa@gmail.com>
2024-11-13 11:51:42 +0900

    editor 1113

--- assets/js/single.js
+++ assets/js/single.js
@@ -16,7 +16,7 @@ if(urlSearch.has('id')){
         author.innerHTML = data.author;
         date.setAttribute('datetime', data.date);
         date.innerHTML = data.date;
-        thumbnail.src = `./images/${data.thumbnail}`;
+        thumbnail.src = `${data.thumbnail}`;
         content.innerHTML = `<iframe id="only-iframe" src=\"./posts/${postId}.html\" />`;
 
         const iframe = document.getElementById("only-iframe");
